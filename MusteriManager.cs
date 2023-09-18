a7e0afa993954dff785ca761ac330173be89d35f
alpherver <alper.tugrulla@gmail.com>
2023-09-18 16:11:42 +0300

    clone

---
+++ MusteriManager.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassMetotDemo
+{
+    internal class MusteriManager
+    {
+        private List<Musteri> musteriListesi = new List<Musteri>();
+
+        public void musteriEkle(Musteri musteri)
+        {
+            musteriListesi.Add(musteri);
+        }
+        public List<Musteri> MusteriListele()
+        {
+            return musteriListesi;
+        }
+
+        public void MusteriSil(int musteriId)
+        {
+            foreach ( Musteri musteri in musteriListesi ) 
+            {
+                if (musteri.musteriId == musteriId)
+                {
+                    musteriListesi.Remove(musteri);
+                    break;
+                }
+
+            }       
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace ClassMetotDemo
+{
+    class ClassMetotDemo
+    {
+        static void Main(string[] args)
+        {
+            Musteri musteri1 = new Musteri();
+            musteri1.musteriId = 1;
+            musteri1.musteriAdi = "Alper";
+            musteri1.musteriSoyad = "Tugrul";
+
+            MusteriManager musteriManager = new MusteriManager();
+            musteriManager.musteriEkle(musteri1);
+
+            int musteriId = musteri1.musteriId;
+            string musteriAdi = musteri1.musteriAdi;
+            string musteriSoyad = musteri1.musteriSoyad;
+
+
+            foreach (Musteri musteri in musteriManager.MusteriListele())
+            {
+            
+                Console.WriteLine("Id: {0}, Ad: {1}, Soyad: {2}", musteriId, musteriAdi , musteriSoyad);
+            }
+
+            // Musteri silme
+            musteriManager.MusteriSil(musteriId);
+
+            // Musteri listeleme
+            foreach (Musteri musteri in musteriManager.MusteriListele())
+            {
+                Console.WriteLine("Id: {0}, Ad: {1}, Soyad: {2}", musteriId, musteriAdi, musteriSoyad);
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+}
